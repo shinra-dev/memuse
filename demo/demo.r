93e55ba9aff7cd475d399557e39a213ff92f1e71
wrathematics <wrathematics@gmail.com>
2013-05-24 08:54:17 -0400

    05.24.13 name change

--- demo/demo.r
+++ demo/demo.r
@@ -1,17 +1,17 @@
 ### Basic construction and object options
-library(fs, quiet=T)
+library(memuse, quiet=T)
 
 linebreak <- function() cat(sprintf("\n"))
 
 # The best unit for representation is chosen by default
-fs(2000)
-fs(2000, unit="kib")
+mu(2000)
+mu(2000, unit="kib")
 
 linebreak()
 
 
 # Construction is case insensitive; we always choose the correct case
-fs(120, unit="gIb", unit.prefix="iec", unit.names="SHorT")
+mu(120, unit="gIb", unit.prefix="iec", unit.names="SHorT")
 
 linebreak()
 
@@ -20,8 +20,8 @@ linebreak()
 # assumption is the prefix is correct but the unit is being misused
 # (as is commonly the case).  So passing unit="kb" with unit.prefix="IEC"
 # will make the appropriate correction:
-fs(2000, unit="kb")
-fs(2000, unit="kb", unit.names="long")
+mu(2000, unit="kb")
+mu(2000, unit="kb", unit.names="long")
 
 linebreak()
 
@@ -29,17 +29,17 @@ linebreak()
 # We can change the default unit.prefix and unit.names
 .NAMES <- "long"
 .PREFIX <- "SI"
-fs(120, unit="kb")
+mu(120, unit="kb")
 
 .PREFIX <- "IEC"
-fs(120, unit="kb")
+mu(120, unit="kb")
 
 .NAMES <- "short"
 
 linebreak()
 
 # We can easily convert between different units
-x <- fs(120, unit="kib")
+x <- mu(120, unit="kib")
 print(x)
 swap.unit(x, "kb")
 swap.unit(x, "b")
