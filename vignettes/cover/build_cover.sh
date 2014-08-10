47e8cfdb604dfb0eb685ab209777599cb372209f
Drew Schmidt <schmidt@math.utk.edu>
2014-08-10 19:57:55 -0400

    added new stuff to vignette

--- vignettes/cover/build_cover.sh
+++ vignettes/cover/build_cover.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+PKGVER=`grep "Version:" ../../DESCRIPTION | sed -e "s/Version: //"`
+sed -i -e "s/packageversion}{[0-9][.][0-9]-[0-9]}/packageversion}{${PKGVER}}/" cover.tex
+
+
 xelatex cover.tex
 xelatex cover.tex
 Rscript -e "tools::compactPDF('.', gs_quality='ebook')"
