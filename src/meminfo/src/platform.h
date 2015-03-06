2b4c6095ff47fe4c862f71d9afda6a4c662383ac
wrathematics <wrathematics@gmail.com>
2015-03-06 17:47:28 -0500

    fixed platform bugs

--- src/meminfo/src/platform.h
+++ src/meminfo/src/platform.h
@@ -8,19 +8,15 @@
 
 
 // "portability"
-#define OS_LINUX (defined(__gnu_linux__) || defined(__linux__) || defined(__linux) || defined(linux))
-
-#define OS_WINDOWS (defined(_WIN32) || defined(__WIN32__) || defined(_WIN64) || defined(__TOS_WIN__) || defined(__WINDOWS__))
-
-#define OS_MAC ((defined(__APPLE__) && defined(__MACH__)) || macintosh || Macintosh)
-
-#define OS_FREEBSD defined(__FreeBSD__)
+//#define CYGWIN_OR_MINGW (defined(__MINGW32__) || defined(__MINGW64__) || defined(__CYGWIN__) || defined(__CYGWIN32__))
+#define OS_LINUX (defined(__gnu_linux__) || defined(__linux__) || defined(__linux))
+#define OS_WINDOWS (defined(__WIN32) || defined(__WIN32__) || defined(_WIN64) || defined(__WIN64) || defined(__WIN64__) || defined(__TOS_WIN__) || defined(__WINNT) || defined(__WINNT__))
+#define OS_MAC (defined(__APPLE__) && defined(__MACH__))
+#define OS_FREEBSD (defined(__FreeBSD__))
 #define OS_BSD (defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__bsdi__) || defined(__DragonFly__))
 #define OS_SOLARIS (defined(__sun) || defined(sun))
 #define OS_HURD (defined(__GNU__) || defined(__gnu_hurd__)) // why the hell not
-
-#define OS_NIX (defined(OS_BSD) || defined(OS_HURD) || defined(OS_LINUX) || defined(OS_MAC) || defined(OS_SOLARIS))
-
+#define OS_NIX (OS_BSD || OS_HURD || OS_LINUX || OS_MAC || OS_SOLARIS)
 
 
 #include <stdint.h>
