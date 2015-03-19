290e15e23148b2303d2925689cef5de6a15aa28d
wrathematics <wrathematics@gmail.com>
2015-03-18 20:51:42 -0400

    removed merkhet demos

--- demo/sys.r
+++ demo/sys.r
@@ -20,14 +20,3 @@ cat("\n")
 Sys.cachelinesize()
 cat("\n")
 
-### How long has your system been up (seconds since boot)?
-Sys.uptime()
-cat("\n")
-
-### How long has this R session been running?
-Sys.runtime()
-cat("\n")
-
-### post-hot system.time()
-post.system.time()
-cat("\n")
