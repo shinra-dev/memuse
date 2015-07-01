e214c4f95fd457b2516c3c55e9fc476194991aeb
wrathematics <wrathematics@gmail.com>
2015-07-01 19:13:39 -0400

    update copyright, fix cover rebuilding, rebuild vignette

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+
+### Fix versioning in vignette, rebuild cover
 PKGVER=`grep "Version:" ../DESCRIPTION | sed -e "s/Version: //"`
 sed -i -e "s/myversion{.*}/myversion{${PKGVER}}/" memuse-guide.Rnw
 
+cd cover
+sed -i -e "s/packageversion}{.*}/packageversion}{${PKGVER}}/" cover.tex
+./build_cover
+cd ..
 
+
+### Rebuild vignette
 rm *.aux *.bbl *.blg *.log *.out *.toc
 
 pdflatex memuse-guide.Rnw
