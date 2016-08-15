beb9043311e2b48f5aa24d403b1834a33cc2ae35
Drew Schmidt <wrathematics@gmail.com>
2016-08-15 17:18:32 -0400

    update licensing

--- src/meminfo/src/cacheinfo.c
+++ src/meminfo/src/cacheinfo.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2015, Schmidt.  All rights reserved.
+/* Copyright (c) 2014-2016, Schmidt.  All rights reserved.
  * Use of this source code is governed by a BSD-style license
  * that can be found in the LICENSE file. */
 
