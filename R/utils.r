e719873d8847f7f7dae8315a73b24fc03473fe31
Drew Schmidt <wrathematics@gmail.com>
2017-04-13 15:14:41 -0400

    minor

--- R/utils.r
+++ R/utils.r
@@ -2,6 +2,7 @@
 mu_error <- function() stop("Badly formed 'memuse' object", call.=FALSE)
 
 
+
 check_type <- function(type, intsize)
 {
   if (type == "double")
@@ -13,11 +14,13 @@ check_type <- function(type, intsize)
 }
 
 
+
 # Function to grab the printable units from the convoluted .units data structure
 get_units <- function(x)
 {
   return( .units[[x@unit.names]][[x@unit.prefix]][["print"]] )
 }
+
 
 
 # Get the correct power for the factor that determines which unit is to be used
@@ -32,6 +35,7 @@ get_power <- function(x)
   else
     mu_error()
 }
+
 
 
 # Convert a memuse object to one stored in bytes
@@ -56,6 +60,7 @@ convert_to_bytes <- function(x)
   
   return( x )
 }
+
 
 
 # Find the best unit representation for an object
@@ -99,6 +104,7 @@ best.unit <- function(x)
 }
 
 
+
 # get the control unit
 ct.unit <- function(x)
 {
@@ -117,6 +123,7 @@ ct.unit <- function(x)
 }
 
 
+
 # stable a*b^c with c an integer
 abc <- function(a, b, c)
 {
@@ -129,6 +136,7 @@ abc <- function(a, b, c)
   
   return( p )
 }
+
 
 
 # whole number checker
@@ -174,3 +182,67 @@ digits2zero <- function(str)
   return( ret )
 }
 
+
+
+# ------------------------------------------------------------------------------
+# Parallel utilities
+# ------------------------------------------------------------------------------
+
+# determine the optimal grid size (ICTXT=0)
+optimal.grid <- function(nprocs)
+{
+  n <- floor(sqrt(nprocs))
+  
+  for (i in 0:(n-1)){
+    ncols <- n-i
+    nrows <- floor(nprocs %% ncols)
+    
+    if (nrows == 0)
+      break
+  }
+  
+  nrows <- nprocs / ncols
+  
+  return(c(nrows, ncols))
+}
+
+
+
+# determine the local dimensions
+numroc <- function(nprocs, dim, bldim, ICTXT=0)
+{
+  MYP <- c(0L, 0L)
+  
+  if (ICTXT==0)
+    PROCS <- optimal.grid(nprocs)
+  else if (ICTXT==1)
+    PROCS <- c(1, nprocs)
+  else if (ICTXT==2)
+    PROCS <- c(nprocs, 1)
+  else 
+    stop("ICTXT must be 0, 1, or 2")
+  
+  ISRCPROC <- 0
+  
+  ldim <- numeric(2)
+  for (i in 1:2){
+    MYDIST <- (PROCS[i] + MYP[i] - ISRCPROC) %% PROCS[i]
+    NBLOCKS <- floor(dim[i] / bldim[i])
+    ldim[i] <- floor(NBLOCKS / PROCS[i]) * bldim[i]
+    EXTRABLKS <- NBLOCKS %% PROCS[i]
+    
+    if (is.na(EXTRABLKS))
+      EXTRABLKS <- 0
+    
+    if (MYDIST < EXTRABLKS)
+      ldim[i] <- ldim[i] + bldim[i]
+    else if (MYDIST == EXTRABLKS)
+      ldim[i] <- ldim[i] + dim[i] %% bldim[i]
+  }
+  
+  if (any(is.na(ldim)))
+    ldim[which(is.na(ldim))] <- 0L
+  if (any(ldim<1)) ldim <- c(0L, 0L)
+  
+  return(ldim)
+}
