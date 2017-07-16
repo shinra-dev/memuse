2cdb90c3b39cb36ac9b1d974ec3ce65efc2f5d7f
wrathematics <wrathematics@gmail.com>
2017-07-16 10:01:35 -0400

    tweak validation messages

--- R/00-classes.r
+++ R/00-classes.r
@@ -3,20 +3,23 @@ valid.mu <- function(object)
   if (!class(object) == "memuse")
     return( "Not a class 'memuse' object" )
   
+  if (object@size < 0)
+    return("invalid slot 'size'; must be >= 0")
+
   object@unit.names <- tolower(object@unit.names)
   if ( !(object@unit.names %in% c("short", "long")) )
-    return( "invalid slot 'unit.names'.  See help('memuse')" )
+    return( "invalid slot 'unit.names'.  See ?memuse" )
   
   object@unit.prefix <- toupper(object@unit.prefix)
   if ( !(object@unit.prefix %in% c("IEC", "SI")) )
-    return( "invalid slot 'unit.prefix'.  See help('memuse')" )
+    return( "invalid slot 'unit.prefix'.  See ?memuse" )
   
   unit <- tolower(object@unit)
   if ( !(unit %in% .units[["short"]][["IEC"]][["check"]]) &&
        !(unit %in% .units[["short"]][["SI"]][["check"]]) &&
        !(unit %in% .units[["long"]][["IEC"]][["check"]]) &&
        !(unit %in% .units[["long"]][["SI"]][["check"]]) )
-    return( "invalid slot 'unit'.  See help('memuse')" )
+    return( "invalid slot 'unit'.  See ?memuse" )
 }
 
 
